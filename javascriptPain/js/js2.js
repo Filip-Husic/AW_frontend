eceeda2fef0ab0df7726ea7de95ab5a2df9168d6
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-11-28 12:14:41 +0100

    change element by id

--- javascriptPain/js/js2.js
+++ javascriptPain/js/js2.js
@@ -4,16 +4,34 @@ let paragraphs = document.getElementsByTagName("p");
 console.log(paragraphs);
 
 //paragraphs[0]
-document
-    .getElementsByTagName("p")[0]
-    .textContent="Deutsche Telekom";
-console
-    .log(
-        document
-        .getElementsByTagName("p")[0]
-        .textContent
-    );
+// document
+//     .getElementsByTagName("p")[0]
+//     .textContent="Deutsche Telekom";
+// console
+//     .log(
+//         document
+//         .getElementsByTagName("p")[0]
+//         .textContent
+//     );
+//
+// document
+//     .getElementsByTagName("p")[0]
+//     .innerHTML="<strong><em>Deutsche Telekom</em></strong>";
 
+//click event
+//onclick event handler
+let paragraph = document.getElementsByTagName("p")[0];
+let previousContent = paragraph.textContent;
 document
-    .getElementsByTagName("p")[0]
-    .innerHTML="<strong><em>Deutsche Telekom</em></strong>";
+    .getElementById("btChange")
+    .onclick=function () {
+    if (paragraph.textContent === "Deutsche Telekom"){
+        paragraph.textContent = previousContent;
+    }else {
+    document
+        .getElementsByTagName("p")[0]
+        .innerHTML="<strong><em>Deutsche Telekom</em></strong>";
+    }
+
+
+};
