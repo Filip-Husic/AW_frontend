53bbad4f6257bc9790dfafbb0a8ded38369b0280
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-11-30 10:32:02 +0100

    get json file and show titles in console

--- javascriptPain/js/js7.js
+++ javascriptPain/js/js7.js
@@ -4,7 +4,61 @@
 
 // <books></books> use custom tags
 
-let person = {
-    name: "Filip Husic",
-    country: "Croatia"
-};
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const xhr = new XMLHttpRequest();
+
+console.log(xhr.readyState); // 0 - unsent
+
+
+xhr.onreadystatechange = function () {
+    console.log(this.readyState);
+
+    // 1 - Opened
+    // 2 - Headers received
+    // 3 - Loading
+    // 4 - Done
+
+    if (this.readyState === 4) {
+
+        // HTTP status codes
+        // 1xx - Info
+        // 2xx - OK
+        // 3xx - redirect / caching
+        // 4xx - client errors
+        // 5xx - server errors
+
+        console.log(this.status);
+        switch (this.status) {
+            case 200: {
+                console.log("OK");
+                //We can receive text, HTML, JSON or XML from the server
+                // JSON.parse converts JSON string to an array
+                let posts = JSON.parse(this.responseText);
+                for (let post of posts) {
+                    console.log(post.title);
+                }
+                break;
+            }
+            case 404: {
+                console.log("Client errors!!!!");
+                break;
+            }
+            default: {
+                console.log("ready state = " + this.readyState);
+            }
+        }
+    }
+    console.log("End");
+}
+
+console.log("Before opening");
+
+
+xhr.open("GET",url);
+
+console.log("Before sending");
+
+xhr.send();
+
+// console.log("End");
