fd64fd2a2aefed9be0941dd0ecdafcfa49ab960d
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-11-30 09:44:32 +0100

    ajax, json and xml, also one exercise from canvas added

---
+++ javascriptPain/js/js7.js
@@ -0,0 +1,10 @@
+// AJAX - Asynchronous JavaScript And XML
+// based on JS object called XMLHttpRequest
+// XML - eXtended Markup Language
+
+// <books></books> use custom tags
+
+let person = {
+    name: "Filip Husic",
+    country: "Croatia"
+};
