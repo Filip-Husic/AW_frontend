4e93eb3d3230a87a1b8329cb596d8114f50a64e1
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-12-01 11:33:09 +0100

    search auto by input

--- javascriptPain/js/js7Fetch.js
+++ javascriptPain/js/js7Fetch.js
@@ -6,6 +6,7 @@ let startingRecord = 0;
 const numberRecordsPerPage = 25;
 let totalNumberRecords;
 
+//TODO IMPLEMENT SEARCH IN PAGINATION
 function paginate(start) {
     let urlValue = url+"?_start="+start+"&_limit="+numberRecordsPerPage;
     //fetch returns a promise
@@ -26,6 +27,7 @@ function paginate(start) {
                 // console.log(post.title);
                 content += `<tr data-id="${post.id}">`;
                 // content += `<td><a href="../html/js7Details.html?id=${post.id}">${post.userId}</a></td>`;
+                // noinspection JSUnresolvedVariable
                 content += `<td>${post.userId}</td>`;
                 content += `<td>${post.title}</td>`;
                 content += `<td>${post.body}</td>`;
@@ -48,7 +50,7 @@ console.log(tablePosts);
 tablePosts.addEventListener("click",function (e) {
     // console.log(e.target.nodeName);
     if (e.target.nodeName === "TD"){
-        console.log("somebody toucha my spaghet!");
+        console.log("Somebody toucha my spaghet!");
         let id = e.target.parentElement.getAttribute("data-id");
         // console.log(id);
         window.location.href=`../html/js7Details.html?id=${id}`;
@@ -77,4 +79,43 @@ document.querySelector("#nextBt").addEventListener("click",function () {
 document.querySelector("#lastBt").addEventListener("click",function () {
     startingRecord = totalNumberRecords - numberRecordsPerPage;
     paginate(startingRecord);
+});
+document.querySelector("#search").addEventListener("input", function() {
+    let value=document.querySelector("#search").value.trim();
+    // if (value==="") {
+    //     alert("You must provide at least one character to search.")
+    //     document.querySelector("#search").focus();
+    // }
+        let urlValue=url + "?title_like=" + value;
+        fetch(urlValue)
+            .then(response => {
+                if (!response.ok) {
+                    return Promise.reject("Page does not exist!");
+                } else {
+                    return response.json();
+                }
+            })
+            .then(posts => {
+                // console.log(posts);
+                if (posts.length === 0){
+                    document.querySelector("#noResults").style.display="block";
+                    document.querySelector(".dataTable").style.display="none";
+                }else {
+                let content="";
+                for(let post of posts) {
+                    content +=`<tr data-id="${post.id}">`;
+                    // noinspection JSUnresolvedVariable
+                    content +=`<td>${post.userId}</td>`;
+                    content +=`<td>${post.title}</td>`;
+                    content +=`<td>${post.body}</td>`;
+                    content +='</tr>';
+                }
+                document.querySelector(".dataTable tbody").innerHTML=content;
+
+                document.querySelector("#noResults").style.display="none";
+                document.querySelector(".dataTable").style.display="block";
+                }
+            })
+            .catch(error => console.log("An error has occurred: "+ error));
+
 });
